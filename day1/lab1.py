b842028e2c0cddb880d9420d763fae469ef87fd0
Michelle Torres <smtorres@wustl.edu>
2014-08-04 15:25:39 -0500

    Lab 1

--- day1/lab1.py
+++ day1/lab1.py
@@ -1,14 +1,41 @@
-def binarify(num): 
-  """convert positive integer to base 2"""
-  if num<=0: return '0'
-  digits = []
-  return ''.join(digits)
+def binarify(num): 	
+	if num<=0: return '0'
+	power=0
+	while num % 2**power<num:
+		y = num % 2**power
+		power = power + 1
+		x = power -1
+	length = range(0, power)
+	coefs = length[::-1]
+	digits = length
+	for i in coefs:
+		temp_power = 2**i
+		bin_num = num / temp_power
+		digits[i] = str(num / temp_power)
+		num = num % temp_power
+	digits = digits[::-1]
+	
+	return ''.join(digits)
 
 def int_to_base(num, base):
-  """convert positive integer to a string in any base"""
-  if num<=0:  return '0' 
-  digits = []
-  return ''.join(digits)
+	"""convert positive integer to a string in any base"""
+	if num<=0:  return '0' 
+  	power=0
+	while num % base**power<num:
+		y = num % base**power
+		power = power + 1
+		x = power -1
+	length = range(0, power)
+	coefs = length[::-1]
+	digits = length
+	for i in coefs:
+		temp_power = base**i
+		bin_num = num / temp_power
+		digits[i] = str(num / temp_power)
+		num = num % temp_power
+	digits = digits[::-1]
+  
+	return ''.join(digits)
 
 def base_to_int(string, base):
   """take a string-formatted number and its base and return the base-10 integer"""
