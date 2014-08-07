c5977f51358a3a938b2681f8fcbed05cb0f04394
Michelle Torres <smtorres@wustl.edu>
2014-08-07 02:46:18 -0500

    Commit 2

--- Assignment01/school.py
+++ Assignment01/school.py
@@ -2,18 +2,23 @@ class School():
 	def __init__(self, school_name):
 		self.school_name = school_name
 		self.db = {}
+		self.dict = {}
 
 
 	def add(self, name, grade):
-			dict_names = {name}
-			dict_names.extend == name
-			self.db = {grade : dict_names}
-	 
+		self.dict = {name}
+		if name in self.db:
+			self.db[grade].add(name)
+		else:
+			temp_dict = {grade : self.dict}
+	 		self.db.update(temp_dict)
 
 
 school = School("Haleakala Hippy School")
 print school.db
 school.add("Aimee", 2)
+school.add("Mark", 3)
+school.add("Sophie", 4)
 print school.db
 
 
