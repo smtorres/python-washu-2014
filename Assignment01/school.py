61c9aa85e738cccca5ac0a540341e35171262afa
Michelle Torres <smtorres@wustl.edu>
2014-08-07 02:14:28 -0500

    Assignment 1
    Not complete! Commit 1

---
+++ Assignment01/school.py
@@ -0,0 +1,23 @@
+class School():
+	def __init__(self, school_name):
+		self.school_name = school_name
+		self.db = {}
+
+
+	def add(self, name, grade):
+			dict_names = {name}
+			dict_names.extend == name
+			self.db = {grade : dict_names}
+	 
+
+
+school = School("Haleakala Hippy School")
+print school.db
+school.add("Aimee", 2)
+print school.db
+
+
+
+
+		
+
