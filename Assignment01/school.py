2281f354d5f1f67de5c0d9996e10375828cf81fe
Michelle Torres <smtorres@wustl.edu>
2014-08-07 03:29:59 -0500

    Commit 3

--- Assignment01/school.py
+++ Assignment01/school.py
@@ -1,25 +1,57 @@
+from collections import OrderedDict
+
 class School():
 	def __init__(self, school_name):
 		self.school_name = school_name
 		self.db = {}
-		self.dict = {}
 
 
 	def add(self, name, grade):
-		self.dict = {name}
-		if name in self.db:
+		if grade in self.db:
 			self.db[grade].add(name)
 		else:
-			temp_dict = {grade : self.dict}
-	 		self.db.update(temp_dict)
+			self.db[grade] = {name}
+
+	def grade(self, grade):
+		if grade not in self.db.keys():
+			return None
+		else:
+			return self.db.get(grade)
+		print self.db.keys
+
+	def sort(self):
+		new_dic = self.db
+		new_dic = OrderedDict(sorted(self.db(), key=lambda x: x[1]))
 
 
-school = School("Haleakala Hippy School")
-print school.db
-school.add("Aimee", 2)
-school.add("Mark", 3)
-school.add("Sophie", 4)
-print school.db
+
+    #def test_sort_school(self):
+     #   self.school.add("Jennifer", 4)
+      #  self.school.add("Kareem", 6)
+       # self.school.add("Christopher", 4)
+        #self.school.add("Kyle", 3)
+        #sorted_students = {
+         #   3: ("Kyle",),
+          #  4: ("Christopher", "Jennifer",),
+           # 6: ("Kareem",)
+       # }
+        #self.assertEqual(sorted_students, self.school.sort())
+
+school = School("RANDOM")
+school.add("James", 2)
+school.add("Blair", 2)
+school.add("Paul", 2)
+school.add("Maria", 1)
+school.add("Andy", 3)
+test = school.db
+print len(test)
+print test.keys()
+x = test.values()
+y = x[1] 
+print y.sorted()
+
+
+
 
 
 
